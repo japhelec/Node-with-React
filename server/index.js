6e2ff024ca7f67cdbf17705a2017d3f75fc7e0e0
Kuei <material3679@gmail.com>
2020-04-26 16:39:15 +0800

    prepare to deploy on Heroku

--- server/index.js
+++ server/index.js
@@ -1,10 +1,12 @@
-//server side common JS module system
-const express = require("express");
+const express = require("express");  //server side common JS module system
 
-const app = express();
+const app = express();  //start an app, there may be more than one app
 
-app.get("/", (req, res) => {
-  res.send("hi: 'there'");
-});
+app.get("/", (req, res) => {            //set up a route handler, focus on HTTP get request
+  res.send({hi: 'there'});              //req, request, containing info about client and request
+});                                     //res, respond, tell node server what info to send back to client
 
-app.listen(5000);
+const PORT = process.env.PORT  || 5000  //Heroku tells us what port to listen 
+app.listen(PORT);                       //tell node to help listen on port PORT
+
+
